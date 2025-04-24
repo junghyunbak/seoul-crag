45dcc8fb9fbb7513f0e3be8329d8f81eec3e0ba5
junghyunbak <jeong5728@gmail.com>
2025-04-24 16:29:00 +0900

    fix: 올바른 useDrag 동작을 위한 스타일 추가

--- frontend/src/components/CragDetailModal/index.tsx
+++ frontend/src/components/CragDetailModal/index.tsx
@@ -112,6 +112,7 @@ export function CragDetailModal() {
               width: '100%',
               height: '100%',
               overflowY: 'auto',
+              touchAction: 'none',
             }}
             ref={scrollRef}
           >
