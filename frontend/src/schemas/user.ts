11ff266463239a876586ab01a52313ea728f9033
junghyunbak <jeong5728@gmail.com>
2025-04-25 07:20:50 +0000

    fix: 사용자 스키마 올바르게 수정

--- frontend/src/schemas/user.ts
+++ frontend/src/schemas/user.ts
@@ -5,8 +5,8 @@ import { rolesScheme } from '@/schemas/role';
 export const userScheme = z.object({
   id: z.string(),
   username: z.string(),
-  email: z.string().optional(),
-  profile_image: z.string().optional(),
+  email: z.union([z.string(), z.null()]).optional(),
+  profile_image: z.union([z.string(), z.null()]).optional(),
   created_at: z.coerce.date(),
   roles: rolesScheme,
 });
