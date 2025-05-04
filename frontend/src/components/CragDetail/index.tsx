fe707827fd0359b93140fc19d5ad6fd472269d4b
junghyunbak <jeong5728@gmail.com>
2025-05-05 00:31:28 +0900

    style: 암장 상세 오 -> 왼으로 등작하도록 수정

--- frontend/src/components/CragDetail/index.tsx
+++ frontend/src/components/CragDetail/index.tsx
@@ -111,9 +111,9 @@ function CragDetailContent({ onClose, crag, images }: CragDetailContentProps) {
 
   return (
     <motion.div
-      initial={{ y: '100%' }}
-      animate={{ y: 0 }}
-      exit={{ y: '100%' }}
+      initial={{ x: '100%' }}
+      animate={{ x: 0 }}
+      exit={{ x: '100%' }}
       transition={{ duration: 0.2 }}
       style={{
         position: 'fixed',
