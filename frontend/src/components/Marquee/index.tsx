c29ffa3c8e667a89be0c9993d4d72ecbe1919074
junghyunbak <jeong5728@gmail.com>
2025-05-05 12:03:24 +0000

    chore: 공지 텍스트에 이메일 추가

--- frontend/src/components/Marquee/index.tsx
+++ frontend/src/components/Marquee/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Marquee from 'react-fast-marquee';
 
 const noticeTexts = [
-  `📢 이미지에 출처를 표기했지만 문제가 될 경우 즉시 삭제하겠습니다. 댓글로 알려주세요.`,
+  `📢 이미지에 출처를 표기했지만 문제가 될 경우 즉시 삭제하겠습니다. 이메일(jeong5728@gmail.com)로 문의주세요.`,
   `📅 세팅 날짜, 휴무일 정보는 인스타그램을 참고하여 수기로 정리한 내용입니다. 실제 관리자가 지정되기 전까지는 반드시 한 번 더 확인해 주세요.`,
 ];
 
