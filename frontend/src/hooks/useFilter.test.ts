69e6d0443929e0a2edbebefcf411f0df0fd63be0
junghyunbak <jeong5728@gmail.com>
2025-05-08 04:45:02 +0000

    test: 운영시간 관련 테스트코드 추가

--- frontend/src/hooks/useFilter.test.ts
+++ frontend/src/hooks/useFilter.test.ts
@@ -15,6 +15,35 @@ const mockCrag: Crag = {
   updated_at: new Date(),
 };
 
+const dateStr = '2025-05-08';
+const timeStr = '09:03:00';
+const dateTimeStr = `${dateStr}T${timeStr}`;
+const today = new DateService(dateTimeStr).date;
+
+const todayCloseOpeningHour: OpeningHour = {
+  id: '',
+  day: DAYS_OF_WEEK[today.getDay()],
+  open_time: timeStr,
+  close_time: timeStr,
+  is_closed: true,
+};
+
+const todayCloseSchedule: Schedule = {
+  id: '',
+  type: 'closed',
+  open_date: dateTimeStr,
+  close_date: dateTimeStr,
+  created_at: new Date(),
+};
+
+const todayReduceSchedule: Schedule = {
+  id: '',
+  open_date: dateTimeStr,
+  close_date: dateTimeStr,
+  type: 'reduced',
+  created_at: new Date(),
+};
+
 describe('이미지 타입 존재여부 검사', () => {
   it('shower 타입의 이미지가 존재할 경우 hasShower가 true여야 한다.', () => {
     const { result } = renderHook(() => {
@@ -37,42 +66,61 @@ describe('이미지 타입 존재여부 검사', () => {
   });
 });
 
+describe('기본 운영 일정에 따른 상태 검사.', () => {
+  it('현재 시간이 운영 일정 밖일 경우 isOff 상태가 true여야 한다.', () => {
+    const { result } = renderHook(() => {
+      const { isOff } = useFilter(
+        {
+          ...mockCrag,
+          openingHourOfWeek: [
+            {
+              ...todayCloseOpeningHour,
+              open_time: '07:00:00',
+              close_time: '08:00:00',
+              is_closed: false,
+            },
+          ],
+        },
+        today
+      );
+
+      return { isOff };
+    });
+
+    expect(result.current.isOff).toBe(true);
+  });
+
+  it('현재 시간이 운영 일정 내일 경우 isOff 상태가 false여야 한다.', () => {
+    const { result } = renderHook(() => {
+      const { isOff } = useFilter(
+        {
+          ...mockCrag,
+          openingHourOfWeek: [
+            {
+              ...todayCloseOpeningHour,
+              open_time: '07:00:00',
+              close_time: '10:00:00',
+              is_closed: false,
+            },
+          ],
+        },
+        today
+      );
+
+      return { isOff };
+    });
+
+    expect(result.current.isOff).toBe(false);
+  });
+});
+
 describe('스케줄에 따른 암장 상태 검사', () => {
-  const dateStr = '2025-05-08';
-  const timeStr = '09:03:00';
-  const dateTimeStr = `${dateStr}T${timeStr}`;
-  const today = new DateService(dateTimeStr).date;
-
-  const todayCloseOpeningHour: OpeningHour = {
-    id: '',
-    day: DAYS_OF_WEEK[today.getDay()],
-    open_time: timeStr,
-    close_time: timeStr,
-    is_closed: true,
-  };
-
-  const todayCloseSchedule: Schedule = {
-    id: '',
-    type: 'closed',
-    open_date: dateTimeStr,
-    close_date: dateTimeStr,
-    created_at: new Date(),
-  };
-
-  const todayReduceSchedule: Schedule = {
-    id: '',
-    open_date: dateTimeStr,
-    close_date: dateTimeStr,
-    type: 'reduced',
-    created_at: new Date(),
-  };
-
   it('오늘 날짜가 정기 휴무일 경우, isOff 상태가 true여야 한다.', () => {
     const { result } = renderHook(() => {
       const { isOff } = useFilter(
         {
           ...mockCrag,
-          openingHourOfWeek: [todayCloseOpeningHour],
+          openingHourOfWeek: [{ ...todayCloseOpeningHour, is_closed: true }],
         },
         today
       );
@@ -104,7 +152,7 @@ describe('스케줄에 따른 암장 상태 검사', () => {
       const { isOff } = useFilter(
         {
           ...mockCrag,
-          openingHourOfWeek: [todayCloseOpeningHour],
+          openingHourOfWeek: [{ ...todayCloseOpeningHour, is_closed: true }],
           futureSchedules: [todayCloseSchedule],
         },
         today
@@ -121,7 +169,7 @@ describe('스케줄에 따른 암장 상태 검사', () => {
       const { isOff } = useFilter(
         {
           ...mockCrag,
-          openingHourOfWeek: [todayCloseOpeningHour],
+          openingHourOfWeek: [{ ...todayCloseOpeningHour, is_closed: true }],
           futureSchedules: [todayReduceSchedule],
         },
         today
