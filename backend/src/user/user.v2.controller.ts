ea31476b6548e9f55a667d58debae4d2bfc4d4d3
jeong57281 <jeong5728@gmail.com>
2025-05-23 16:26:50 +0900

    fix: me 라우터 위치 조절

--- backend/src/user/user.v2.controller.ts
+++ backend/src/user/user.v2.controller.ts
@@ -3,16 +3,40 @@ import {
   Controller,
   Get,
   Param,
+  Req,
+  UnauthorizedException,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-
+import { type Request } from 'express';
 import { UserV2Service } from './user.v2.service';
 import { UserResponseDto } from './dto/user-response.dto';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { isJwtParsedUser } from 'src/utils/typeguard';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('v2/users')
 export class UserV2Controller {
   constructor(private readonly userService: UserV2Service) {}
+
+  @Get()
+  async getUsers() {
+    const users = await this.userService.getUsers();
+
+    return users?.map((user) => new UserResponseDto({ ...user }));
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@Req() req: Request) {
+    const { user } = req;
+
+    if (!user || !isJwtParsedUser(user)) {
+      throw new UnauthorizedException('로그인 되어있지 않습니다.');
+    }
+
+    return this.userService.getUser(user.id);
+  }
 
   @Get(':id')
   async getUser(@Param('id') userId: string) {
@@ -21,12 +45,5 @@ export class UserV2Controller {
     return new UserResponseDto({
       ...user,
     });
-  }
-
-  @Get()
-  async getUsers() {
-    const users = await this.userService.getUsers();
-
-    return users?.map((user) => new UserResponseDto({ ...user }));
   }
 }
