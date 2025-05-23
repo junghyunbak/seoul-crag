5ecf5111ef322cedb4f2a177fd1ca1a62cbbe383
jeong57281 <jeong5728@gmail.com>
2025-05-23 13:37:00 +0900

    chore: react-query 에러도 캡처되도록 수정

--- frontend/src/router/QueryProvider.tsx
+++ frontend/src/router/QueryProvider.tsx
@@ -1,10 +1,16 @@
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient, DefaultError } from '@tanstack/react-query';
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { urlService } from '@/utils';
 
-function throwOnError() {
+import * as Sentry from '@sentry/react';
+
+function throwOnError(error: DefaultError) {
+  console.error(error);
+
+  Sentry.captureException(error);
+
   if (window.location.pathname.startsWith(urlService.getAbsolutePath('/manage'))) {
     return true;
   }
