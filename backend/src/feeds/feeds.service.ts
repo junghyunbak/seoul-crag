7b03db6ddf7fa99d66ae220f112ce2763aee3589
jeong57281 <jeong5728@gmail.com>
2025-05-27 15:31:38 +0900

    feat: 피드 크롤링 구현

--- backend/src/feeds/feeds.service.ts
+++ backend/src/feeds/feeds.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Feed } from './feeds.entity';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { GymsV2Service } from 'src/gyms/gyms.v2.service';
+import puppeteer from 'puppeteer';
 
 @Injectable()
 export class FeedsService {
@@ -24,13 +25,65 @@ export class FeedsService {
     return this.feedRepo.save(feed);
   }
 
-  @Cron(CronExpression.EVERY_MINUTE)
+  @Cron(CronExpression.EVERY_2_HOURS)
   async handleCron() {
     const gyms = await this.gymsV2Service.findAll();
 
-    console.log(
-      '테스트',
-      gyms.map((gym) => gym.name),
-    );
+    const browser = await puppeteer.launch({
+      headless: false,
+      userDataDir: './puppeteer_session',
+    });
+
+    const page = await browser.newPage();
+
+    for (const gym of gyms) {
+      const url = gym.website_url;
+
+      try {
+        await page.goto(url, {
+          waitUntil: 'networkidle2',
+        });
+
+        await page.waitForSelector('a[href*="/p/"]');
+
+        const anchors = await page.$$eval('a[href*="/p/"]', (anchors) =>
+          anchors.map((anchor) => ({
+            href: anchor.href,
+            thumbnailSrc: anchor.querySelector('img')?.src || '',
+          })),
+        );
+
+        for (const { href, thumbnailSrc } of anchors) {
+          const exists = await this.existsPost(href);
+
+          if (!exists) {
+            const feed = this.feedRepo.create({
+              url: href,
+              is_read: false,
+              thumbnail_url: thumbnailSrc,
+              gym: gym,
+            });
+
+            await this.feedRepo.save(feed);
+          }
+        }
+
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(true);
+          }, 2000);
+        });
+      } catch (error) {
+        console.log('페이지를 가져오지 못했습니다.', error);
+      }
+    }
+
+    await browser.close();
+  }
+
+  async existsPost(href: string): Promise<boolean> {
+    const post = await this.feedRepo.findOne({ where: { url: href } });
+
+    return !!post;
   }
 }
