c57a54f72955e079708de5b425fc2a5cf9c37c56
junghyunbak <jeong5728@gmail.com>
2025-05-31 10:07:39 +0900

    refactor: 불필요한 훅 제거

--- frontend/src/components/StoryImage/index.tsx
+++ frontend/src/components/StoryImage/index.tsx
@@ -9,7 +9,7 @@ import { ImageWithSource } from '@/components/ImageWithSource';
 
 import { QUERY_STRING } from '@/constants';
 
-import { useFetchCrag, useFetchImages } from '@/hooks';
+import { useFetchCrag } from '@/hooks';
 
 import { AnimatePresence } from 'framer-motion';
 
@@ -32,8 +32,7 @@ export default function StoryImage({ imageType }: StoryImageProps) {
 
   const { crag } = useFetchCrag({ cragId });
 
-  // TODO: crag 객체 내 imageType -> images가 된다면 필요없어질 fetch
-  const { images } = useFetchImages(cragId, imageType);
+  const images = (crag?.images || []).filter((image) => image.type === imageType);
 
   /**
    * preload images
