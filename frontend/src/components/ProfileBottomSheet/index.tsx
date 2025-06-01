13b4bc6e4aef8f1f7695e69f470f2186bc7f12e1
junghyunbak <jeong5728@gmail.com>
2025-06-01 23:52:57 +0900

    fix: 리스트렌더링 누락 키값 추가

--- frontend/src/components/ProfileBottomSheet/index.tsx
+++ frontend/src/components/ProfileBottomSheet/index.tsx
@@ -120,9 +120,18 @@ export function ProfileBottomSheet() {
 
                 <Typography variant="h6">기여한 암장</Typography>
 
+                {/**
+                 * 한 암장에 두개 이상의 기여가 가능하기 때문에, 키값에 인덱스값 까지 포함.
+                 *
+                 * // TODO: 중복 기여 암장 제거 예정
+                 */}
                 <AvatarGroup total={user.gymUserContributions.length}>
-                  {user.gymUserContributions.map(({ gym }) => {
-                    return <Avatar src={gym.thumbnail_url || ''}>{gym.name[0]}</Avatar>;
+                  {user.gymUserContributions.map(({ gym }, i) => {
+                    return (
+                      <Avatar key={`${gym.id}-${i}`} src={gym.thumbnail_url || ''}>
+                        {gym.name[0]}
+                      </Avatar>
+                    );
                   })}
                 </AvatarGroup>
               </Box>
