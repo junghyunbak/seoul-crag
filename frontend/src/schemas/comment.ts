5603d21c7840f3ba4fbb21a53957aff45fd28bc3
junghyunbak <jeong5728@gmail.com>
2025-06-02 00:08:39 +0900

    fix: 댓글 모듈 사용자 스키마 수정

--- frontend/src/schemas/comment.ts
+++ frontend/src/schemas/comment.ts
@@ -1,11 +1,11 @@
-import { userSchema } from '@/schemas/user';
+import { InternalUserSchema } from '@/schemas/user';
 import { z } from 'zod';
 
 export const commentScheme = z.object({
   id: z.string().uuid(),
   content: z.string(),
   is_admin_only: z.boolean(),
-  user: userSchema,
+  user: InternalUserSchema,
   created_at: z.coerce.date(),
   updated_at: z.coerce.date(),
 });
