0f8abc8f20c17dbc53167831209734c781aae262
junghyunbak <jeong5728@gmail.com>
2025-06-03 10:44:02 +0900

    refactor: 공지 marquee 분자로 분리

--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -9,6 +9,7 @@ import { MapOptions } from '@/components/molecules/MapOptions';
 import { MenuTrigger } from '@/components/molecules/MenuTrigger';
 import { NoticeList } from '@/components/molecules/NoticeList';
 import { NoticeListItem } from '@/components/molecules/NoticeListItem';
+import { NoticeMarquee } from '@/components/molecules/NoticeMarquee';
 import { NoticeModalTrigger } from '@/components/molecules/NoticeModalTrigger';
 import { SearchAndLoading } from '@/components/molecules/SearchAndLoading';
 import { SearchInputWithRemove } from '@/components/molecules/SearchInputWithRemove';
@@ -34,4 +35,5 @@ export const Molecules = {
   MapOptions,
   NoticeList,
   NoticeListItem,
+  NoticeMarquee,
 };
