2cd5208fb2d9f33dbeb6ce64c1667c7e71d59522
junghyunbak <jeong5728@gmail.com>
2025-06-04 18:41:20 +0900

    fix: 오래된 번들 캐시로 인한 오류 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        cleanupOutdatedCaches: true,
         navigateFallback: '/index.html',
         navigateFallbackDenylist: [/^\/api\//, /^\/uploads\//],
       },
