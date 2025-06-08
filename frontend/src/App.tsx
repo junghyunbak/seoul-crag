e32083022c42913655d7dec0d096f3e6d3e73d5e
junghyunbak <jeong5728@gmail.com>
2025-06-08 10:29:02 +0900

    feat: 업데이트 & reload confirm 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,29 @@
+import { useEffect } from 'react';
+
 import { RouterProvider } from 'react-router';
 
 import { router } from '@/router';
 
+import { registerSW } from 'virtual:pwa-register';
+
 function App() {
+  useEffect(() => {
+    const updateSW = registerSW({
+      onNeedRefresh() {
+        const shouldReload = confirm('새 버전이 배포되었습니다. 새로고침할까요?');
+        if (shouldReload) {
+          updateSW(true); // skipWaiting() 실행 → 새 SW 활성화
+          window.location.reload(); // 페이지 리로드
+        }
+      },
+      onOfflineReady() {
+        console.log('PWA 오프라인 사용 준비 완료!');
+      },
+    });
+
+    return () => updateSW?.unregister?.();
+  }, []);
+
   return <RouterProvider router={router} />;
 }
 
